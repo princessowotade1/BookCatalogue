96d8381a33b4bc72a4c8595f1196fb539e561565
Princess Owotade <73714558+princessowotade1@users.noreply.github.com>
2023-08-14 20:10:51 -0500

    Started Search production and changed code in Navigation to handle search

---
+++ ProjectFiles/src/Pages/Search/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import terms from "../../Components/Navigation"
+
+
+
+function SearchPage(props) {
+    console.log(props)
+
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default SearchPage
